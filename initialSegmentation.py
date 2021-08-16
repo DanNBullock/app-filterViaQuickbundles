f63ec4d1283364420106a0539778b20894aa4cc2
DanNBullock <dnbulloc@umail.iu.edu>
2021-08-16 12:58:00 -0500

    fixed output directory structure, fixed previous fixes

--- initialSegmentation.py
+++ initialSegmentation.py
@@ -9,6 +9,7 @@ from dipy.io.streamline import save_tractogram
 import seaborn as sns
 import matplotlib.pyplot as plt
 import copy
+import os
 
 def jsonParseNPass():
     # load config.json structure, modified because of windows filepath issues
@@ -23,46 +24,46 @@ def jsonParseNPass():
         streamThresh=config['minStreamsInCluster']
         return reference, tractogram, streamThresh
 
-    def cullViaClusters(clusters,tractogram,streamThresh):
-        clustersSurviveThresh=np.greater(np.asarray(list(map(len, clusters))),streamThresh)
-        survivingStreams=[]
-        for iclusters in clusters[clustersSurviveThresh]:
-            survivingStreams=survivingStreams + iclusters.indices
-        culledStreamIndicies=list(set(list(range(1,len(tractogram.streamlines))))-set(survivingStreams))
-        culledTractogram=copy.deepcopy(tractogram)
-        culledTractogram.streamlines=culledTractogram.streamlines[culledStreamIndicies]
-        #cull those streamlines
-        #don't know what to do about those warnings
-        tractogram.streamlines=tractogram.streamlines[survivingStreams]
-        return tractogram, culledTractogram
+def cullViaClusters(clusters,tractogram,streamThresh):
+    clustersSurviveThresh=np.greater(np.asarray(list(map(len, clusters))),streamThresh)
+    survivingStreams=[]
+    for iclusters in clusters[clustersSurviveThresh]:
+        survivingStreams=survivingStreams + iclusters.indices
+    culledStreamIndicies=list(set(list(range(1,len(tractogram.streamlines))))-set(survivingStreams))
+    culledTractogram=copy.deepcopy(tractogram)
+    culledTractogram.streamlines=culledTractogram.streamlines[culledStreamIndicies]
+    #cull those streamlines
+    #don't know what to do about those warnings
+    tractogram.streamlines=tractogram.streamlines[survivingStreams]
+    return tractogram, culledTractogram
     
-    #obtain rquisite files from config.json
-    [reference, tractogram, streamThresh]=jsonParseNPass()
-    #threshold heuristic designed to target a threshold of 15 at 500k streamlines
-    #not actually accurate as relationship isn't linear
-    #inputStreamNumber=len(tractogram.streamlines)
-    #qbThresh=np.divide(inputStreamNumber,33333)
-    qbThresh=15
-    #perform quickBundles
-    qb = QuickBundles(threshold=qbThresh)
-    clusters = qb.cluster(tractogram.streamlines)
-    
-    #perform cull
-    [outTractogram,culledTractogram]=cullViaClusters(clusters,tractogram,streamThresh)
-    
-    #save tractograms
-    save_tractogram(outTractogram, 'track.tck')
-    save_tractogram(culledTractogram, 'culled.tck')
-    
-    numberCulled=inputStreamNumber-len(outTractogram.streamlines)
-    print(str(numberCulled) + ' streamlines culled')
-    #create output diagnostic plots
-    sns.histplot(data=np.asarray(list(map(len, clusters))),log_scale=True, binwidth=.1)
-    #I dont know why I have to enter it in for both values
-    plt.plot([streamThresh,streamThresh], [  1,100], linewidth=4,color='red')
-    plt.savefig('clusterCullHist.png')
-    #not working, fix later
-    #plt.xlabel='cluster size'
-    #plt.title='cluster size counts'
-    
-    # end
+#obtain rquisite files from config.json
+[reference, tractogram, streamThresh]=jsonParseNPass()
+#threshold heuristic designed to target a threshold of 15 at 500k streamlines
+#not actually accurate as relationship isn't linear
+inputStreamNumber=len(tractogram.streamlines)
+#qbThresh=np.divide(inputStreamNumber,33333)
+qbThresh=15
+#perform quickBundles
+qb = QuickBundles(threshold=qbThresh)
+clusters = qb.cluster(tractogram.streamlines)
+
+#perform cull
+[outTractogram,culledTractogram]=cullViaClusters(clusters,tractogram,streamThresh)
+
+#save tractograms
+save_tractogram(outTractogram, os.path.join('output_tck','track.tck'))
+save_tractogram(culledTractogram, os.path.join('output_tck2','culled.tck'))
+
+numberCulled=inputStreamNumber-len(outTractogram.streamlines)
+print(str(numberCulled) + ' streamlines culled')
+#create output diagnostic plots
+sns.histplot(data=np.asarray(list(map(len, clusters))),log_scale=True, binwidth=.1)
+#I dont know why I have to enter it in for both values
+plt.plot([streamThresh,streamThresh], [  1,100], linewidth=4,color='red')
+plt.savefig(os.path.join('output_tck','clusterCullHist.png'))
+#not working, fix later
+#plt.xlabel='cluster size'
+#plt.title='cluster size counts'
+
+# end
